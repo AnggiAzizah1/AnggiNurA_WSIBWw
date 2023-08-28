599c1c2fe5906fa52ac9bb59ffcdbaff19c65c04
AnggiAzizah1 <anggi.ajijah@gmail.com>
2023-08-29 06:51:49 +0700

    First Upload

---
+++ contoh/src/contoh/bike9.java
@@ -0,0 +1,21 @@
+package contoh;
+
+/**
+ *
+ * @author anggi nur azizah
+ */
+public class bike9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    final int speed=90;
+    void run (){
+        speed=400;
+    }
+    public static void main(String[] args) {
+    bike9 obj=new bike9();
+    obj.run();
+    }
+    
+}
